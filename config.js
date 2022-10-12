fd1709491a3db5b1311349127a40ded4da265cf4
RIYANJERMANN <114304908+RIYANJERMANN@users.noreply.github.com>
2022-10-12 23:32:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -50,28 +50,28 @@ global.sdc = 'https://discord.gg/WEJQjugTY7'
 global.snh = 'https://youtube.com/c/LeonGanz/'
 
 /*============== PAYMENT ==============*/
-global.pdana = '085608625102'
-global.povo = '085608625102'
-global.pgopay = '085608625102'
-global.ppulsa = '085608625102'
-global.ppulsa2 = '085608625102'
-global.psaweria = 'https://saweria.com/LeonGanz'
+global.pdana = '087786201972'
+global.povo = '087786201972'
+global.pgopay = '087786201972'
+global.ppulsa = '_'
+global.ppulsa2 = '_'
+global.psaweria = '_'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6282127104362'
-global.nomorown = '6282127104362'
-global.nomorpart = '6282127104362'
-global.namebot = 'Yusuf Oct'
-global.nameown = 'Yusuf Oct'
+global.nomorbot = '082124572932'
+global.nomorown = '79998112307'
+global.nomorpart = '79998112307'
+global.namebot = 'zi bot'
+global.nameown = 'zizi'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127104362', 'Yusuf Oct', true],
-  ['6285608625102', 'Rama Agung', true],
-  ['6282351319893', 'Muhimmah Putri', true],
-  ['6283847909854', 'Zaky Rashif', true],
-  ['6287877173955', 'Ferdiz-Afk', true]
+  ['79998112307', 'zizi', true],
+  ['0000', 'malaikat raqib', true],
+  ['0000', 'malaikat atid', true],
+  ['0000', 'pahala', true],
+  ['0000', 'zizi anti rip', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
